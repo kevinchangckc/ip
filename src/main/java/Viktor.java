38f1c29310eaef39cb92b9d3eabfe7eff8bdb464
kevinchangckc <>
2025-02-18 11:18:45 +0800

    completed Level-6

--- src/main/java/Viktor.java
+++ src/main/java/Viktor.java
@@ -356,6 +356,7 @@ class ViktorException extends Exception {
     }
 }
 
+/*
 public class Viktor {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -430,6 +431,119 @@ public class Viktor {
 
                 else {
                     throw new ViktorException("I'm afraid don't understand your command. Please try again.");
+                }
+
+            } catch (ViktorException e) {
+                System.out.println("_________________________________________");
+                System.out.println(e.getMessage());
+                System.out.println("_________________________________________");
+            }
+        }
+
+        scanner.close();
+    }
+
+
+ */
+// Level-6
+public class Viktor {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<Task> tasks = new ArrayList<>();
+
+        System.out.println("_________________________________________");
+        System.out.println("Hi there. I'm Viktor.");
+        System.out.println("How can I help you?");
+        System.out.println("_________________________________________");
+
+        while (true) {
+            String input = scanner.nextLine();
+
+            try {
+                if (input.equalsIgnoreCase("bye")) {
+                    System.out.println("_________________________________________");
+                    System.out.println("Goodbye! Have a great day. 😊");
+                    System.out.println("_________________________________________");
+                    break;
+                }
+
+                else if (input.equalsIgnoreCase("list")) {
+                    System.out.println("_________________________________________");
+                    System.out.println("Here are your tasks:");
+                    for (int i = 0; i < tasks.size(); i++) {
+                        System.out.println((i + 1) + ". " + tasks.get(i));
+                    }
+                    System.out.println("_________________________________________");
+                }
+
+                else if (input.startsWith("delete ")) {
+                    String[] parts = input.split(" ");
+                    if (parts.length != 2) {
+                        throw new ViktorException("Oops! Please specify a valid task number to delete.");
+                    }
+
+                    try {
+                        int taskIndex = Integer.parseInt(parts[1]) - 1;
+                        if (taskIndex < 0 || taskIndex >= tasks.size()) {
+                            throw new ViktorException("Oops! Task number does not exist.");
+                        }
+
+                        Task removedTask = tasks.remove(taskIndex);
+                        System.out.println("_________________________________________");
+                        System.out.println("Noted. I've removed this task:");
+                        System.out.println(removedTask);
+                        System.out.println("Now you have " + tasks.size() + " tasks in the list.");
+                        System.out.println("_________________________________________");
+
+                    } catch (NumberFormatException e) {
+                        throw new ViktorException("Oops! Task number must be a valid number.");
+                    }
+                }
+
+                else if (input.startsWith("todo")) {
+                    String taskDescription = input.substring(4).trim();
+                    if (taskDescription.isEmpty()) {
+                        throw new ViktorException("Oops! A todo task cannot be empty.");
+                    }
+                    Task newTask = new ToDo(taskDescription);
+                    tasks.add(newTask);
+                    System.out.println("_________________________________________");
+                    System.out.println("Got it. I've added this task:");
+                    System.out.println(newTask);
+                    System.out.println("Now you have " + tasks.size() + " tasks in the list.");
+                    System.out.println("_________________________________________");
+                }
+
+                else if (input.startsWith("deadline")) {
+                    String[] parts = input.substring(8).split(" /by ");
+                    if (parts.length < 2) {
+                        throw new ViktorException("Oops! Please specify a deadline using '/by'.");
+                    }
+                    Task newTask = new Deadline(parts[0], parts[1]);
+                    tasks.add(newTask);
+                    System.out.println("_________________________________________");
+                    System.out.println("Got it. I've added this task:");
+                    System.out.println(newTask);
+                    System.out.println("Now you have " + tasks.size() + " tasks in the list.");
+                    System.out.println("_________________________________________");
+                }
+
+                else if (input.startsWith("event")) {
+                    String[] parts = input.substring(5).split(" /from | /to ");
+                    if (parts.length < 3) {
+                        throw new ViktorException("Oops! Please use '/from' and '/to' for event timing.");
+                    }
+                    Task newTask = new Event(parts[0], parts[1], parts[2]);
+                    tasks.add(newTask);
+                    System.out.println("_________________________________________");
+                    System.out.println("Got it. I've added this task:");
+                    System.out.println(newTask);
+                    System.out.println("Now you have " + tasks.size() + " tasks in the list.");
+                    System.out.println("_________________________________________");
+                }
+
+                else {
+                    throw new ViktorException("Oops! I don't understand that command. Try again.");
                 }
 
             } catch (ViktorException e) {
